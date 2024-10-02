70f4e6fa2ca66c8aac5ca9f441746291d31f4aa0
OgulcanSevinc <ogulcan.sevinc.student.hepl.be>
2024-10-02 13:01:49 +0200

    feature:Cmake

---
+++ build/BD_Books/CMakeFiles/CreationBD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CreationBD.dir/CreationBD.cpp.o"
+  "CreationBD"
+  "CreationBD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CreationBD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/ClientBookEncoderQt/CMakeFiles/ClientBookEncoderQtApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientBookEncoderQtApp.dir/main.cpp.o"
+  "CMakeFiles/ClientBookEncoderQtApp.dir/mainwindowclientbookencoder.cpp.o"
+  "CMakeFiles/ClientBookEncoderQtApp.dir/moc_mainwindowclientbookencoder.cpp.o"
+  "ClientBookEncoderQtApp"
+  "ClientBookEncoderQtApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientBookEncoderQtApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
